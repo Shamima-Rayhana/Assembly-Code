11a1a77e4547e5be985bc0cdc01a3a67e9edbdbb
Shamima-Rayhana <85000140+Shamima-Rayhana@users.noreply.github.com>
2022-05-12 12:41:08 +0600

    Adding two multi byte numbers

--- MultiByteAddition.asm
+++ MultiByteAddition.asm
@@ -1,18 +1,21 @@
+;Auther: Shamima Rayhana Rumi
+;Date: 12-05-2022
+;Content: MultiByte addition
 
 
-MOV SI,1000H
-MOV DI,1011H
+MOV SI,1000H             ;SI->05
+MOV DI,1011H             ;DI->00
 MOV BP,1021H
-MOV CL,[SI]
-INC SI
+MOV CL,[SI]              ;[SI]->05 so, CL->05
+INC SI                   ;SI->1001H
 MOV BX,0000H
 MOV DL,0000H
-CLC
+CLC                      ;clear carry flag, CF->0
 
-REPEAT:
-MOV AL,[SI+BX]
-ADC AL,[DI+BX]
-MOV [BP],AL
+REPEAT:                  ;Example:
+MOV AL,[SI+BX]           ;   F5 C2 64 72 17  [First number]
+ADC AL,[DI+BX]           ; + C2 65 75 07 12  [secomd number]
+MOV [BP],AL              ; 1 B8 27 D9 79 29  [result ofter addition]
 INC BX
 INC BP
 LOOP REPEAT
