97085a4364d6933f6cd3ea03f2d7576363cdc529
Shamima-Rayhana <85000140+Shamima-Rayhana@users.noreply.github.com>
2022-05-22 21:04:20 +0600

    Subtraction of two numbers

---
+++ Subtraction.asm
@@ -0,0 +1,19 @@
+;Auther: Shamima Rayhana Rumi
+;Date: 22-05-2022
+;Content: Subtraction
+
+;Subtraction 
+
+
+MOV AL,05H
+MOV BL,04H
+
+SUB AL,BL
+HLT
+
+;MOV AX,1102H
+;MOV BX,1100H
+;MUL BX
+;HLT
+
+
